0b32a02d749dac6d38ddec88ac77b196bd404f45
Evgeny <buze@ya.ru>
2021-11-22 18:10:28 +0800

    Initial Commit

---
+++ MenuWithSlide/MenuWithSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuWithSlideApp.swift
+//  MenuWithSlide
+//
+//  Created by Evgeny on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MenuWithSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
