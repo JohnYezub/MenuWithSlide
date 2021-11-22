114536b2906f07fe7a32022d402d6353107ec4ce
Evgeny <buze@ya.ru>
2021-11-22 18:14:12 +0800

    add code

--- MenuWithSlide/ContentView.swift
+++ MenuWithSlide/ContentView.swift
@@ -7,15 +7,82 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AreaWithGesture: View {
+    @State private var valueX: CGFloat = 0
+    @State private var isShown = false
+    private let offsetAnimation = Animation.easeIn(duration: 0.5)
+    private let timerInterval = TimeInterval(0.6)
+
     var body: some View {
-        Text("Hello, world!")
+        ZStack(alignment: .topTrailing) {
+
+            Button {
+                withAnimation(offsetAnimation) {
+                    isShown = true
+                }
+            } label: {
+                Image(systemName: "slider.horizontal.3")
+                    .resizable()
+                    .frame(width: 30, height: 25)
+            }
             .padding()
+
+            GeometryReader { proxy in
+                ZStack {
+                    // Background view
+                    Color.black.opacity(isShown ? 0.5 : 0)
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            withAnimation(offsetAnimation) {
+                                valueX = proxy.size.width
+                                DispatchQueue.main.asyncAfter(deadline: .now() + timerInterval) {
+                                    isShown = false
+                                }
+                            }
+                        }
+
+                    if isShown {
+                        let dragGesture = DragGesture()
+                            .onChanged { value in
+                                if value.translation.width > 0 {
+                                    valueX = value.translation.width
+                                }
+                            }
+                            .onEnded { _ in
+                                withAnimation(offsetAnimation) {
+                                    if valueX < proxy.size.width / 2 {
+                                        valueX = 0
+                                    } else {
+                                        valueX = proxy.size.width
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + timerInterval) {
+                                            isShown = false
+                                        }
+                                    }
+                                }
+                            }
+
+                        // The menu view
+                        HStack {
+                            Spacer()
+                            Color.red
+                                .overlay(Text("\(valueX)"))
+                                .frame(width: proxy.size.width * 0.8, height: proxy.size.height)
+                                .offset(x: valueX)
+                                .gesture(dragGesture)
+                        }.onAppear {
+                            withAnimation(offsetAnimation) {
+                                valueX = 0
+                            }
+                        }
+                    } // if
+                } // end of Z
+            } // geo
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ViewWithDragGesture_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AreaWithGesture()
     }
 }
